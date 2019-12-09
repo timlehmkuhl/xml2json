7cf3dc63b89876ae3852c5f34564684d432b42a4
Tim <tim.lehmkuhl@stud.th-luebeck.de>
2019-12-09 16:56:01 +0100

    Inlay

--- src/main/java/XML/GenerateJson.java
+++ src/main/java/XML/GenerateJson.java
@@ -48,25 +48,34 @@ public class GenerateJson   {
 
         @Override
         public void enterArray(XMLParser.ArrayContext ctx) {
-
+            ST inlay = templates.getInstanceOf("inlay");
             ST pairarray = null;
            if(ctx.pair().size() > 0){
                pairarray = templates.getInstanceOf("pairarray");
            } else   {
                pairarray =  templates.getInstanceOf("emptyarray");
            }
-
+            boolean flag = false;
             String name = ctx.getParent().getChild(0).getText().replace("<", "").replace(">", "");
             pairarray.add("name", name);
+            inlay.add("name", name);
             int i = 0;
            for(XMLParser.PairContext pair : ctx.pair()){
                String arrayname = ctx.pair().get(i).getChild(0).getText().replace("<", "").replace(">", "");
                names.add(arrayname);
                i++;
+               if(!arrayname.equals("element")){
+                   System.err.println(arrayname);
+                   ST inlaypair = templates.getInstanceOf("inlaypair");
+                   inlaypair.add("subname", arrayname).add("content", pair.getChild(1).getText());
+                   flag = true;
+                  inlay.add("element", inlaypair);
+               } else
                pairarray.add("content", pair.getChild(1).getText());
 
            }
-            all.add("element", pairarray);
+           if(flag)  all.add("element", inlay);
+            else all.add("element", pairarray);
         }
     }
 
